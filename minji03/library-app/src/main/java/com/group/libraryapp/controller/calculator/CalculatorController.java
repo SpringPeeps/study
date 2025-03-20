b3b6f0987be6b79c9c290f7878b907e49a35e697
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-20 23:00:27 +0900

    Add : 추가예제(문제1)

--- minji03/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ minji03/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,6 +1,7 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorCalcResponse;
 import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,5 +17,14 @@ public class CalculatorController {
     @PostMapping("/multiply") // POST /multiply
     public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
         return request.getNum1() * request.getNum2();
+    }
+    // 추가 예제 -> 문제 1: 입력된 두 수의 덧셈, 뺄셈, 곱셈 결과값을 JSON 형태로 반환하기
+    @GetMapping("/api/v1/calc")
+    public CalculatorCalcResponse CalculateTwoNumbers(CalculatorAddRequest request) {
+        int sum = request.getNum1() + request.getNum2();
+        int minus = request.getNum1() - request.getNum2();
+        int multiply = request.getNum1() * request.getNum2();
+
+        return new CalculatorCalcResponse(sum, minus, multiply);
     }
 }
