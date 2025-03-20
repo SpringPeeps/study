6592c482b8f4ca48a9d7e79f70107485e066d7ad
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-20 16:36:48 +0900

    Fix : DTO 도입 및 리펙토링

--- minji03/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ minji03/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,13 +1,13 @@
 package com.group.libraryapp.controller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
     @GetMapping("/add") // GET /add
-    public int addTwoNumbers(@RequestParam int num1, @RequestParam int num2) {
-        return num1 + num2;
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNum1() + request.getNum2();
     }
 }
