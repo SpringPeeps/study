b4c7f6776be65346cc1031e4cdf3520015c25c7e
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-20 22:41:18 +0900

    Add : POST /multiply 생성

--- minji03/library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
+++ minji03/library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
@@ -1,10 +1,10 @@
 package com.group.libraryapp.dto.calculator.request;
 
-public class CalculatorAddRequest {
+public class CalculatorMultiplyRequest {
     private int num1;
     private int num2;
 
-    public CalculatorAddRequest(int num1, int num2) { // 강의에는 생성자 추가가 안 되어있음
+    public CalculatorMultiplyRequest(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
