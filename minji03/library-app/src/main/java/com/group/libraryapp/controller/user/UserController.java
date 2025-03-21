3103d849846f66c4cbc3ea3c8db3afa92c589989
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-21 21:04:39 +0900

    Add : PUT /user 추가

--- minji03/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ minji03/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,17 +1,11 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -37,5 +31,11 @@ public class UserController {
             int age = rs.getInt("age");
             return new UserResponse(id, name, age);
         });
+    }
+
+    @PutMapping("/user") // PUT /user
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
     }
 }
