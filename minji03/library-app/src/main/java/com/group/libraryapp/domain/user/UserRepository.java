e4922b466b75d9a70dd73309a568ad5b4f7c3084
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-03-28 15:57:50 +0900

    Add : 책 생성 및 대출 기능 추가

--- minji03/library-app/src/main/java/com/group/libraryapp/domain/user/UserRepository.java
+++ minji03/library-app/src/main/java/com/group/libraryapp/domain/user/UserRepository.java
@@ -2,9 +2,11 @@ package com.group.libraryapp.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     // @Repository 어노테이션 없이도 JpaRepository 를 상속한 것 만으로도 스프링빈으로 관리됨
 
-    // 함수 만들기: 이 때 함수의 이름이 아주 중요함 (함수명을 따라서 쿼리가 작성됨)
-    User findByName(String name);
+    // 함수 만들기: 이 때 함수의 이름이 중요함 (함수명을 따라서 쿼리가 작성됨)
+    Optional<User> findByName(String name);
 }
---
+++ minji03/library-app/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,26 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import javax.persistence.*;
+
+@Entity
+public class UserLoanHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column
+    private Long userId;
+
+    @Column
+    private String bookName;
+
+    @Column
+    private boolean isReturn; // 0 == false, 1 == true로 자동으로 매핑!
+
+    protected UserLoanHistory() {};
+    public UserLoanHistory(Long userId, String bookName) {
+        this.userId = userId;
+        this.bookName = bookName;
+        this.isReturn = false;
+    }
+}
